5bc0c1184cab163d4322edb7339578a35b3cf939
zach powers <zcharlop@rockefeller.edu>
2015-04-14 14:32:32 -0400

    name fixes and add barcode errors

--- parallel_split_library.py
+++ parallel_split_library.py
@@ -30,8 +30,9 @@ from fastq_concat import isgzip
 #@click.option('--discardbadwindows/--no-discardbadwindows', default=True, help="whether to drop the entire bad sequence or not")
 @click.option('--ncpus', type=click.INT, default=4, help="number of cpus to use")
 @click.option('--qualwindow', type=click.INT, default=30, help="number of cpus to use")
+@click.option('--barcodeerrors', type=click.INT, default=1, help="maximum allows erros in the barcode")
 def parallel_split_library(fasta, qual, outfile, mappingfile, barcodetype,qual_cutoff, logfile,
-                                  splitsize, splitlibrarycommand,
+                                  splitsize, splitlibrarycommand,barcodeerrors,
                                   #discardbadwindows,
                                   ncpus, qualwindow):
     """
@@ -58,17 +59,17 @@ def parallel_split_library(fasta, qual, outfile, mappingfile, barcodetype,qual_c
     # generate the split files and check for length equivalency.
     # note that the fasta and qual files are piped through seqtk to make both filetypes into
     # two-line files.
-    print("Splitting the Fasta File, {}".format(fastq))
-    if isgzip(fastq):
-        call("zcat {} | seqtk seq -l0 | split -l {} - {}".format(fastq, splitsize ,"forward_"),shell=True)
+    print("Splitting the Fasta File, {}".format(fasta))
+    if isgzip(fasta):
+        call("zcat {} | seqtk seq -l0 | split -l {} - {}".format(fasta, splitsize ,"forward_"),shell=True)
     else:
-        call("cat {}  | seqtk seq -l0 | split -l {} - {}".format(fastq, splitsize ,"forward_"),shell=True)
+        call("cat {}  | seqtk seq -l0 | split -l {} - {}".format(fasta, splitsize ,"forward_"),shell=True)
 
-    print("Splitting the Qual  File, {}".format(qualfile))
-    if isgzip(qualfile):
-        call("zcat {} | seqtk seq -l0 | split -l {} - {}".format(qualfile, splitsize,"barcode_"),shell=True)
+    print("Splitting the Qual  File, {}".format(qual))
+    if isgzip(qual):
+        call("zcat {} | seqtk seq -l0 | split -l {} - {}".format(qual, splitsize,"barcode_"),shell=True)
     else:
-        call("cat {}  | seqtk seq -l0 | split -l {} - {}".format(qualfile, splitsize,"barcode_"),shell=True)
+        call("cat {}  | seqtk seq -l0 | split -l {} - {}".format(qual, splitsize,"barcode_"),shell=True)
 
     #get the names of the split files and zip them together
     split_files_forward = sorted(glob.glob("forward_*"))
@@ -83,7 +84,7 @@ def parallel_split_library(fasta, qual, outfile, mappingfile, barcodetype,qual_c
     p = multiprocessing.Pool(ncpus)
     handlerfunc = partial(process_split_files, splitlibrarycommand=splitlibrarycommand,
                           mappingfile=mappingfile, qual_cutoff=qual_cutoff, barcodetype=barcodetype,
-                          splitsize=splitsize, qualwindow=qualwindow,
+                          splitsize=splitsize, qualwindow=qualwindow, barcodeerrors=barcodeerrors
                           #,discardbadwindows=discardbadwindows
                           )
 
@@ -114,7 +115,7 @@ def parallel_split_library(fasta, qual, outfile, mappingfile, barcodetype,qual_c
 
 
 def process_split_files(data,splitlibrarycommand, mappingfile, qual_cutoff, barcodetype, splitsize,
-                        qualwindow
+                        qualwindow, barcodeerrors
                         #discardbadwindows
                         ):
     """helper function for use with functional programming.
@@ -129,7 +130,7 @@ def process_split_files(data,splitlibrarycommand, mappingfile, qual_cutoff, barc
                    "-b", barcodetype,
                    "-w", qualwindow,
                    "-q", str(qual_cutoff),
-                   "-e", barcoderrors,
+                   "-e", barcodeerrors,
                    "-n", str(number * (splitsize/4))]
 
     call(command)
