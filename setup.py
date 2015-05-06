b9e3e1ad5fdb8d5e28b1b800a8ffaa4d43fd55a8
zach powers <zcharlop@rockefeller.edu>
2015-05-06 13:25:54 -0400

    add CLI and bump the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='qiime-tools',
-    version='0.1.6',
+    version='0.1.7',
     py_modules=['fastq_concat',
                 'parallel_split_libraries_fastq'],
     install_requires=[
@@ -16,6 +16,6 @@ setup(
         parallel_split_library_fastq=parallel_split_libraries_fastq:parallel_splitlibraries_fastq
         parallel_split_libraries=parallel_split_library:parallel_split_library
         UCfiles_to_taxtable=UCfiles_to_taxtable:UCfiles_to_taxtable
-
+        taxtable_to_otutable=UCfiles_to_taxtable:taxtable_to_otutable
     ''',
 )
