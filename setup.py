9ae3f65e91851f2f966027af09955790d3eb9239
zach powers <zcharlop@rockefeller.edu>
2015-05-20 16:01:48 -0400

    revert to own filehandlgin for now and clen up the concat file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='qiime-tools',
-    version='0.1.17',
+    version='0.1.18',
     py_modules=['fastq_concat',
                 'parallel_split_libraries_fastq'],
     install_requires=[
