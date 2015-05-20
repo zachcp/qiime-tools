3178fc1a89d8bcb96ee5ca318f30098d7fbf8b15
zach powers <zcharlop@rockefeller.edu>
2015-05-20 15:09:02 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='qiime-tools',
-    version='0.1.13',
+    version='0.1.14',
     py_modules=['fastq_concat',
                 'parallel_split_libraries_fastq'],
     install_requires=[
