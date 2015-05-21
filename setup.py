ac36e826f413b6a9c08f563e18d7fb9a8ebb86a3
zach powers <zcharlop@rockefeller.edu>
2015-05-21 15:23:03 -0400

    bump to 0.2.0. Major differences are the beginnings of a test suite a reorganization of the code to allow testing. qiime_tools is now organized in a module format that allows propert import and discovery in the tests.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qiime-tools',
-    version='0.1.25',
+    version='0.2.0',
     install_requires=[
         'Click',
         'Biopython'
