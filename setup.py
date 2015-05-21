0f7c65c5ca439e13bfe34e36575a1e803a951a39
zach powers <zcharlop@rockefeller.edu>
2015-05-21 12:00:34 -0400

    reformat scripts into package format

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='qiime-tools',
-    version='0.1.24',
-    py_modules=['fastq_concat',
-                'parallel_split_libraries_fastq'],
+    version='0.1.25',
     install_requires=[
         'Click',
         'Biopython'
     ],
+    packages=find_packages(),
     entry_points='''
         [console_scripts]
-        fastqconcat=fastq_concat:fastqconcat
-        parallelconcat=fastq_concat:parallel_concat
-        parallel_split_library_fastq=parallel_split_libraries_fastq:parallel_splitlibraries_fastq
-        parallel_split_libraries=parallel_split_library:parallel_split_library
-        UCfiles_to_taxtable=UCfiles_to_taxtable:UCfiles_to_taxtable
-        taxtable_to_otutable=UCfiles_to_taxtable:taxtable_to_otutable
+        fastqconcat = qiime_tools.fastq_concat:fastqconcat
+        parallelconcat = qiime_tools.fastq_concat:parallel_concat
+        parallel_split_library_fastq = qiime_tools.parallel_split_libraries_fastq:parallel_splitlibraries_fastq
+        parallel_split_libraries = qiime_tools.parallel_split_library:parallel_split_library
+        UCfiles_to_taxtable = qiime_tools.UCfiles_to_taxtable:UCfiles_to_taxtable
+        taxtable_to_otutable = qiime_tools.UCfiles_to_taxtable:taxtable_to_otutable
     ''',
 )
