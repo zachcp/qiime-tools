23e14d820f5867dfdc2cd2dbe4f32dd24fe68f2b
zach powers <zcharlop@rockefeller.edu>
2015-05-21 15:55:38 -0400

    change the value of the spacer

--- qiime_tools/fastq_concat.py
+++ qiime_tools/fastq_concat.py
@@ -87,7 +87,7 @@ def process_fastq(fastqs, revcomp, keep_left, keep_right, spacer, spacercharacte
 
         if spacer:
             newseq  = fseq[:keep_left] + spacercharacters + rseq[-keep_right:]
-            newqual = fqual[:keep_left] + "".join(["0" for char in spacercharacters]) + rqual[-keep_right:]
+            newqual = fqual[:keep_left] + "".join(["A" for char in spacercharacters]) + rqual[-keep_right:]
             print("New Qual: {}".format(newqual))
             print(len(newseq), len(newqual))
 
@@ -101,7 +101,7 @@ def process_fastq(fastqs, revcomp, keep_left, keep_right, spacer, spacercharacte
         #put the data together
         if spacer:
             newseq  = fseq[:keep_left] + spacercharacters + rseq[:keep_right]
-            newqual = fqual[:keep_left] + "".join(["0" for char in spacercharacters]) + rqual[:keep_right]
+            newqual = fqual[:keep_left] + "".join(["A" for char in spacercharacters]) + rqual[:keep_right]
             print("New Qual: {}".format(newqual))
             print(len(newseq), len(newqual))
 
