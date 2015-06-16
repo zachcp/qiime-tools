d1c2af7518f1b8f3fd88a7b6f7cfe037358d27d9
zach powers <zcharlop@rockefeller.edu>
2015-06-16 16:27:14 -0400

    CLI RUnner

--- qiime_tools/tests/test_parallel_split_libraries_fastq.py
+++ qiime_tools/tests/test_parallel_split_libraries_fastq.py
@@ -5,6 +5,22 @@ This file test the parallel_split_libraries_fastq script.
 
 
 from subprocess import call, check_call
+from test_fastqconcat import fixname
+from qiime_tools.parallel_split_libraries_fastq import parallel_splitlibraries_fastq
+
+from click.testing import CliRunner
+
+
+trimmed     = fixname("data/trimmeed.fq")
+barcodes    = fixname("data/barcodes.fq")
+mappingfile = fixname("data/MappingFile.txt")
+
+
+# System Utility Tests
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+
 
 def test_coreutils():
     """
@@ -27,3 +43,28 @@ def test_seqtk():
 
     """
     assert(call('seqtk', shell=True) == 1)
+
+
+# Splitting Fastq Tests.
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+
+
+def test_parallel_splitlibraries_fastq():
+    "run parallel_splitlibraries_fastq and check that the sequences have been concatenated."
+    runner=CliRunner()
+    with runner.isolated_filesystem():
+        outfq = "out.fq"
+        result = runner.invoke(parallel_splitlibraries_fastq,
+                               ["--fastq", trimmed,
+                                "--barcode_fastq", barcodes,
+                                "--outfile", outfq,
+                                "--mappingfile", mappingfile,
+                                "--barcodetype", "18",
+                                "--qual_cutoff", "0",
+                                "--splitsize", "100",
+                                "--ncpus", "1"])
+
+        #assert the output is the correct length and sequence
+        assert not result.exception
