4bfba538b2ed0f7047b72e94ffc8fd6bfbeb60d1
zach powers <zcharlop@rockefeller.edu>
2015-06-16 15:32:40 -0400

    add testing using py.test using setuptools integration. you can invoke by running python setup.py test

--- setup.py
+++ setup.py
@@ -1,4 +1,26 @@
-from setuptools import setup, find_packages
+import sys
+from setuptools import setup, find_packages, Command
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 setup(
     name='qiime-tools',
@@ -18,6 +40,8 @@ setup(
         UCfiles_to_taxtable = qiime_tools.UCfiles_to_taxtable:UCfiles_to_taxtable
         taxtable_to_otutable = qiime_tools.UCfiles_to_taxtable:taxtable_to_otutable
     ''',
-    test_requirements = ['pytest>=2.1']
+    test_requirements = ['pytest>=2.1'],
+    tests_require=['pytest'],
+    cmdclass = {'test': PyTest},
 
 )
