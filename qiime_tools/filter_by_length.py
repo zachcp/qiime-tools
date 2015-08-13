eae8169cbdc450e828925cf13dc32202f1e16990
zach powers <zcharlop@rockefeller.edu>
2015-08-13 15:48:12 -0400

    add int type

--- qiime_tools/filter_by_length.py
+++ qiime_tools/filter_by_length.py
@@ -6,16 +6,12 @@ import click
 from Bio import SeqIO
 
 @click.command()
-@click.option('--fastafile', type = click.File('r'), help="inputfastafile")
+@click.option('--fastafile', type = click.Path(exists=True), help="inputfastafile")
 @click.option('--outputfasta',type = click.STRING, help="outputfile")
-@click.option('--length')
+@click.option('--length', type=click.INT)
 def filter_by_length(fastafile, outputfasta, length):
     """
     from an input Fastafile, filter reads not matching the length parameter
     """
-    with open(outputfasta, "w") as f:
-        for rec in SeqIO.parse(fastafile, 'fasta'):
-            if len(rec.seq) == length:
-                f.write(">{}\n{}\n".format(rec.id, rec.seq))
-    #records = (rec for rec in SeqIO.parse(fastafile,'fasta') if len(rec.seq) == length)
-    #SeqIO.write(records, outputfasta,'fasta')
+    records = (rec for rec in SeqIO.parse(open(fastafile,'r'),'fasta') if len(rec.seq) == length)
+    SeqIO.write(records, outputfasta,'fasta')
