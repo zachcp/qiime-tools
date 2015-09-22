55f5292ca29d5bde35bf570b0a7e50e2ecd890eb
zach powers <zcharlop@rockefeller.edu>
2015-09-22 15:02:46 -0400

    add suffix capability

--- qiime_tools/split_fastq_by_name.py
+++ qiime_tools/split_fastq_by_name.py
@@ -12,9 +12,10 @@ from Bio import SeqIO
 
 
 @click.command()
-@click.option('--fastq', type=click.File('r'), prompt=True,help="name of the fastq forward file")
+@click.option('--fastq', type=click.File('r'), prompt=True,help="name of the fastq file")
+@click.option('--suffix', default="", help="suffix to add to name. useful for discriminating Forward/Reverse reads")
 @click.option('--outdir', prompt=True, help="name of the output directory")
-def split_fastq_by_name(fastq,outdir):
+def split_fastq_by_name(fastq,suffix, outdir):
 	"""
 	split a fastqfile based on the names in the header
 
@@ -27,10 +28,10 @@ def split_fastq_by_name(fastq,outdir):
 	for fastq in fastqs:
 		#The record name is {{name}}_{{number}}
 		sample = fastq.name.split("_")[0]
-		outlocation = "{}/{}.fastq".format(outdir,sample)
+		outlocation = "{}/{}{}.fastq".format(outdir, sample, suffix)
 		qualscores = map(str,fastq.letter_annotations['phred_quality'])
 
-
+		print(sample, outlocation)
 		with open(outlocation,'wa') as f:
 			f.write("@{}\n{}\n+\n{}".format(fastq.description,
 											fastq.seq,
