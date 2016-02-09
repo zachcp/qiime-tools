67f75fc3749f182b123eba8159a84bf1308a805b
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-02-09 14:14:55 -0500

    bump

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class PyTest(TestCommand):
 
 setup(
     name='qiime-tools',
-    version='0.4.9.1',
+    version='0.4.10',
     install_requires=[
         'Click >= 0.6.0',
         'Biopython >=1.6.5',
