99976ad314bd83fea2a4f182796b3b3bee62985f
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-02-10 13:40:27 -0500

    add splitting options

--- qiime_tools/split_fasta_by_name.py
+++ qiime_tools/split_fasta_by_name.py
@@ -10,10 +10,13 @@ import click
 @click.command()
 @click.option('--fastafile', type=click.File('r'), prompt=True,help="name of the fasta file")
 @click.option('--outdir', prompt=True, help="name of the output directory")
+@click.option('--spliton', type=click.Choice(['underscore', 'dot']))
 @click.option('--deletedir/--no-deletedir', default=False, help="whether or not to delete the directory")
-def split_fasta_by_name(fastafile,outdir, deletedir):
+def split_fasta_by_name(fastafile,outdir, spliton, deletedir):
     """
-    split a fastqfile based on the names in the header
+    split a fastqfile based on the names in the header. splits on teh
+    record ID using either an underscore or a dot character and
+    taking the first member of the split list.
 
     """
 
@@ -26,7 +29,13 @@ def split_fasta_by_name(fastafile,outdir, deletedir):
     records = SeqIO.parse(fastafile,'fasta')
 
     for rec in records:
-        sample = rec.id.split("_")[0]
+        if spliton == "underscore":
+            sample = rec.id.split("_")[0]
+        elif spliton == "dot":
+            sample = rec.id.split(".")[0]
+        else:
+            raise ValueError("names are split by underscores or dots")
+
         outlocation = "{}/{}.fasta".format(outdir, sample)
         with open(outlocation,'a') as f:
             f.write(">{} {}\n{}\n".format(rec.id, rec.description, str(rec.seq)))
